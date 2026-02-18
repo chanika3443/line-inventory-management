2efa5bf28809ce4dd8328f9e07e0a396c0115ef0
chanika3443 <chanika3443@gmail.com>
2026-02-18 20:45:56 +0700

    Add code splitting to reduce bundle size and remove warnings

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,17 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
-    sourcemap: false
+    sourcemap: false,
+    chunkSizeWarningLimit: 1000, // Increase limit to 1000 kB
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'vendor-react': ['react', 'react-dom', 'react-router-dom'],
+          'vendor-xlsx': ['xlsx'],
+          'vendor-liff': ['@line/liff']
+        }
+      }
+    }
   },
   server: {
     port: 3000,
